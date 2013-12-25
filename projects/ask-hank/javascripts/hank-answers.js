6eb07332c35df460380973c0f2774100e1fa0e5a
steveo1485 <stevennugent@gmail.com>
2013-12-25 09:09:41 -0800

    Add responses to Ask Hank in portfolio

--- projects/ask-hank/javascripts/hank-answers.js
+++ projects/ask-hank/javascripts/hank-answers.js
@@ -3,7 +3,13 @@ function hankAnswers(inputField) {
 	var possibleAnswers = ["I'm not sure, but give me a treat!",
 						   "How should I know? I'm just a dog. A talking dog, but still...",
 						   "Yes! I mean, No! ...maybe? Woof.",
-						   "Let's think about this. I would...ANOTHER DOG! WOOF! WOOF!"]
+						   "Let's think about this. I would...ANOTHER DOG! WOOF! WOOF!",
+						   "Have you thought about chasing a cat instead?",
+						   "I say yes! But I also eat grass.",
+						   "Chase your tail and everything will become clear, I swear.",
+						   "100% NO! Oh, I thought you asked if I wanted a bath...",
+						   "Sorry, can you ask again? I was dreaming of rolling in the grass.",
+						   "Wait! I heard a car door. I need to bark. Ask again later."]
 	var randomNumber = Math.floor(Math.random()*possibleAnswers.length)
 	var randomResponse = possibleAnswers[randomNumber]
 	hankSays.innerText = randomResponse
